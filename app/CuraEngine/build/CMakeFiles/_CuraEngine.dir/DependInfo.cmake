d4961da02d1158cef2abfc825780f765780e4905
Anthony Richard Brown <abbrowna@gmail.com>
2017-10-25 12:52:19 +0300

    Added CuraEngine to source. Cura Engine called depending on platform.

---
+++ app/CuraEngine/build/CMakeFiles/_CuraEngine.dir/DependInfo.cmake
@@ -0,0 +1,102 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/curafiles/CuraEngine/build/Cura.pb.cc" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/Cura.pb.cc.o"
+  "/home/curafiles/CuraEngine/src/ConicalOverhang.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/ConicalOverhang.cpp.o"
+  "/home/curafiles/CuraEngine/src/ExtruderTrain.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/ExtruderTrain.cpp.o"
+  "/home/curafiles/CuraEngine/src/FffGcodeWriter.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/FffGcodeWriter.cpp.o"
+  "/home/curafiles/CuraEngine/src/FffPolygonGenerator.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/FffPolygonGenerator.cpp.o"
+  "/home/curafiles/CuraEngine/src/FffProcessor.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/FffProcessor.cpp.o"
+  "/home/curafiles/CuraEngine/src/GCodePathConfig.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/GCodePathConfig.cpp.o"
+  "/home/curafiles/CuraEngine/src/InsetOrderOptimizer.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/InsetOrderOptimizer.cpp.o"
+  "/home/curafiles/CuraEngine/src/LayerPlan.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/LayerPlan.cpp.o"
+  "/home/curafiles/CuraEngine/src/LayerPlanBuffer.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/LayerPlanBuffer.cpp.o"
+  "/home/curafiles/CuraEngine/src/MergeInfillLines.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/MergeInfillLines.cpp.o"
+  "/home/curafiles/CuraEngine/src/MeshGroup.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/MeshGroup.cpp.o"
+  "/home/curafiles/CuraEngine/src/Mold.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/Mold.cpp.o"
+  "/home/curafiles/CuraEngine/src/Preheat.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/Preheat.cpp.o"
+  "/home/curafiles/CuraEngine/src/PrimeTower.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/PrimeTower.cpp.o"
+  "/home/curafiles/CuraEngine/src/SkirtBrim.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/SkirtBrim.cpp.o"
+  "/home/curafiles/CuraEngine/src/SupportInfillPart.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/SupportInfillPart.cpp.o"
+  "/home/curafiles/CuraEngine/src/TopSurface.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/TopSurface.cpp.o"
+  "/home/curafiles/CuraEngine/src/WallsComputation.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/WallsComputation.cpp.o"
+  "/home/curafiles/CuraEngine/src/Weaver.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/Weaver.cpp.o"
+  "/home/curafiles/CuraEngine/src/Wireframe2gcode.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/Wireframe2gcode.cpp.o"
+  "/home/curafiles/CuraEngine/src/bridge.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/bridge.cpp.o"
+  "/home/curafiles/CuraEngine/src/commandSocket.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/commandSocket.cpp.o"
+  "/home/curafiles/CuraEngine/src/gcodeExport.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/gcodeExport.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/NoZigZagConnectorProcessor.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/NoZigZagConnectorProcessor.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/SpaceFillingTreeFill.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/SpaceFillingTreeFill.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/SpaghettiInfill.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/SpaghettiInfill.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/SpaghettiInfillPathGenerator.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/SpaghettiInfillPathGenerator.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/SubDivCube.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/SubDivCube.cpp.o"
+  "/home/curafiles/CuraEngine/src/infill/ZigzagConnectorProcessor.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/infill/ZigzagConnectorProcessor.cpp.o"
+  "/home/curafiles/CuraEngine/src/layerPart.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/layerPart.cpp.o"
+  "/home/curafiles/CuraEngine/src/mesh.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/mesh.cpp.o"
+  "/home/curafiles/CuraEngine/src/multiVolumes.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/multiVolumes.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathOrderOptimizer.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathOrderOptimizer.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathPlanning/Comb.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathPlanning/Comb.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathPlanning/GCodePath.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathPlanning/GCodePath.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathPlanning/LinePolygonsCrossings.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathPlanning/LinePolygonsCrossings.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathPlanning/NozzleTempInsert.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathPlanning/NozzleTempInsert.cpp.o"
+  "/home/curafiles/CuraEngine/src/pathPlanning/TimeMaterialEstimates.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/pathPlanning/TimeMaterialEstimates.cpp.o"
+  "/home/curafiles/CuraEngine/src/progress/Progress.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/progress/Progress.cpp.o"
+  "/home/curafiles/CuraEngine/src/progress/ProgressStageEstimator.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/progress/ProgressStageEstimator.cpp.o"
+  "/home/curafiles/CuraEngine/src/raft.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/raft.cpp.o"
+  "/home/curafiles/CuraEngine/src/settings/PathConfigStorage.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/settings/PathConfigStorage.cpp.o"
+  "/home/curafiles/CuraEngine/src/settings/SettingConfig.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/settings/SettingConfig.cpp.o"
+  "/home/curafiles/CuraEngine/src/settings/SettingContainer.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/settings/SettingContainer.cpp.o"
+  "/home/curafiles/CuraEngine/src/settings/SettingRegistry.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/settings/SettingRegistry.cpp.o"
+  "/home/curafiles/CuraEngine/src/settings/settings.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/settings/settings.cpp.o"
+  "/home/curafiles/CuraEngine/src/skin.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/skin.cpp.o"
+  "/home/curafiles/CuraEngine/src/sliceDataStorage.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/sliceDataStorage.cpp.o"
+  "/home/curafiles/CuraEngine/src/slicer.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/slicer.cpp.o"
+  "/home/curafiles/CuraEngine/src/support.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/support.cpp.o"
+  "/home/curafiles/CuraEngine/src/timeEstimate.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/timeEstimate.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/AABB.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/AABB.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/AABB3D.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/AABB3D.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/Date.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/Date.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/LinearAlg2D.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/LinearAlg2D.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/ListPolyIt.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/ListPolyIt.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/PolygonProximityLinker.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/PolygonProximityLinker.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/PolygonsPointIndex.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/PolygonsPointIndex.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/ProximityPointLink.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/ProximityPointLink.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/SpaceFillingTree.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/SpaceFillingTree.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/gettime.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/gettime.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/logoutput.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/logoutput.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/polygon.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/polygon.cpp.o"
+  "/home/curafiles/CuraEngine/src/utils/polygonUtils.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/utils/polygonUtils.cpp.o"
+  "/home/curafiles/CuraEngine/src/wallOverlap.cpp" "/home/curafiles/CuraEngine/build/CMakeFiles/_CuraEngine.dir/src/wallOverlap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ARCUS"
+  "VERSION=\"master\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../libs"
+  "/usr/local/include"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/curafiles/CuraEngine/build/Cura.pb.h" "/home/curafiles/CuraEngine/build/Cura.pb.cc"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/curafiles/CuraEngine/build/CMakeFiles/clipper.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- app/stlprocessing.py
+++ app/stlprocessing.py
@@ -1,13 +1,24 @@
 import re
 import subprocess
 import math
+from sys import platform
 
-def slicedweight(pathtofile, density):    
-    cmd = r'cd C:\Program Files\Cura 2.7 && CuraEngine slice -v -j "C:\Program Files\Cura 2.7\resources\definitions\fdmprinter.def.json" -o "C:\Users\abbro\Documents\Visual Studio 2017\Projects\Cubic3D\Cubic3D\output.gcode" -s machine_height=175 -s machine_depth=200 -s machine_width=200 -s material_diameter=1.75 -s center_object=true -s adhesion_type=skirt -s infill_line_distance=2.4 -s support_enable=true -l "{0}"'.format(pathtofile)
-    result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    string = result.decode('utf-8')
-    search = re.compile('(?<=Filament:\s)[0-9]+')
-    filamentvolume = search.findall(string)
-    mass = int(filamentvolume[0])*density/1000
-    roundmass = int(math.ceil(mass))
-    return (string,roundmass)
+def slicedweight(pathtofile, density):  
+    if platform == 'win32':
+        cmd = r'cd C:\Program Files\Cura 2.7 && CuraEngine slice -v -j "C:\Program Files\Cura 2.7\resources\definitions\fdmprinter.def.json" -o "C:\Users\abbro\Documents\Visual Studio 2017\Projects\Cubic3D\Cubic3D\output.gcode" -s machine_height=175 -s machine_depth=200 -s machine_width=200 -s material_diameter=1.75 -s center_object=true -s adhesion_type=skirt -s infill_line_distance=2.4 -s support_enable=true -l "{0}"'.format(pathtofile)
+        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        string = result.decode('utf-8')
+        search = re.compile('(?<=Filament:\s)[0-9]+')
+        filamentvolume = search.findall(string)
+        mass = int(filamentvolume[0])*density/1000
+        roundmass = int(math.ceil(mass))
+        return (string,roundmass)
+    else:
+        cmd = r'/CuraEngine/build/CuraEngine slice -v -j "/CuraEngine/resources/definitions/fdmprinter.def.json" -o "/output.gcode" -s machine_height=175 -s machine_depth=200 -s machine_width=200 -s material_diameter=1.75 -s center_object=true -s adhesion_type=skirt -s infill_line_distance=2.4 -s support_enable=true -l "{0}"'.format(pathtofile)
+        result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        string = result.decode('utf-8')
+        search = re.compile('(?<=Filament:\s)[0-9]+')
+        filamentvolume = search.findall(string)
+        mass = int(filamentvolume[0])*density/1000
+        roundmass = int(math.ceil(mass))
+        return (string,roundmass)
