d5d16ff0463e0fa0606549c133b8afa7798addb7
Anthony Richard Brown <abbrowna@gmail.com>
2017-11-02 21:06:06 +0300

    Corrected Email template

--- app/views.py
+++ app/views.py
@@ -330,25 +330,25 @@ def signup(request):
         }
     )
 
-def startsignup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('upload')
-    else:
-        form = SignUpForm()
-    return render(request,'app/signup.html',
-        {
-            'form': form,
-            'title':'SignUp',
-            'year':datetime.now().year,                            
-        }
-    )
+#def startsignup(request):
+#    if request.method == 'POST':
+#        form = SignUpForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            username = form.cleaned_data.get('username')
+#            raw_password = form.cleaned_data.get('password1')
+#            user = authenticate(username=username, password=raw_password)
+#            login(request, user)
+#            return redirect('upload')
+#    else:
+#        form = SignUpForm()
+#    return render(request,'app/signup.html',
+#        {
+#            'form': form,
+#            'title':'SignUp',
+#            'year':datetime.now().year,                            
+#        }
+#    )
 
 
 
