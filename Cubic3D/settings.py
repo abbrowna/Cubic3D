3711e173c37518a5a6d8da164b5ea74b5b694651
Anthony Richard Brown <abbrowna@gmail.com>
2021-01-19 22:52:40 +0300

    Add google analytics #2

--- Cubic3D/settings.py
+++ Cubic3D/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['139.59.26.37','localhost','cubic3d.co.ke','www.cubic3d.co.ke']
 
+GOOGLE_ANALYTICS = {
+    'google_analytics_id': 'UA-187644512-1',
+}
 
 # Application definition
 INSTALLED_APPS = [
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'google_analytics',
 ]
 
 MIDDLEWARE = [
