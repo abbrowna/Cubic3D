47751229a8f4aaa26049e5ea63de3cbd140264d9
Anthony Richard Brown <abbrowna@gmail.com>
2021-01-30 21:20:58 +0300

    Added email authentication for new users

--- Cubic3D/urls.py
+++ Cubic3D/urls.py
@@ -67,6 +67,8 @@ urlpatterns = [
             ),name='login'
         ),   
     path('signup/',app.views.signup, name='signup'),
+    path('signup/verification/<user_email>',app.views.pending_verification, name='pending_verification'),
+    path('verification/', include('verify_email.urls')),
     path('logout/',django.contrib.auth.views.LogoutView.as_view(), name='logout'),
     
     path('accounts/password_reset/',
