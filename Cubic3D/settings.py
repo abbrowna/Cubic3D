29d8373ffe55c52f2900560e845713d575278201
Anthony Richard Brown <abbrowna@gmail.com>
2021-02-10 16:29:50 +0300

    Change site_Id to 1

--- Cubic3D/settings.py
+++ Cubic3D/settings.py
@@ -14,6 +14,7 @@ import os
 import posixpath
 from sys import platform
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -50,7 +51,7 @@ INSTALLED_APPS = [
     'verify_email',
 ]
 
-#SITE_ID = 1
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
