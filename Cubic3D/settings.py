0ba32f7d4ddab09bf527de29ac598dd43bba2965
Anthony Richard Brown <abbrowna@gmail.com>
2021-05-08 16:44:50 +0300

    Added default wsgi

--- Cubic3D/settings.py
+++ Cubic3D/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.59.26.37','localhost','cubic3d.co.ke','www.cubic3d.co.ke','store.cubic3d.co.ke','www.store.cubic3d.co.ke']
 
@@ -85,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'Cubic3D.wsgi.application'
 
 
 # Database
