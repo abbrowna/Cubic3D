d573991c831b7e2740317a541e3bfdd65d320086
Anthony Richard Brown <abbrowna@gmail.com>
2021-06-01 16:24:04 +0300

    Added Region model to Cubic3D admin

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PrintRequest,ThingOrders,Profile,Quote,Material,Invoice,GroupRecord
+from .models import PrintRequest,ThingOrders,Profile,Quote,Material,Invoice,GroupRecord,Region
 
 admin.site.register(PrintRequest)
 admin.site.register(ThingOrders)
@@ -9,3 +9,4 @@ admin.site.register(Quote)
 admin.site.register(Material)
 admin.site.register(Invoice)
 admin.site.register(GroupRecord)
+admin.site.register(Region)
